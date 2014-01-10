20ad5a73130111e8d839402d82b44ea3e6e27210
Xusen Yin <yinxusen@gmail.com>
2014-01-10 10:41:31 +0800

    change 254 to 255 in case of oob

--- mllib/src/main/scala/org/apache/spark/mllib/classification/LogisticRegression.scala
+++ mllib/src/main/scala/org/apache/spark/mllib/classification/LogisticRegression.scala
@@ -182,7 +182,7 @@ object LogisticRegressionWithSGD {
     }
     val sc = new SparkContext(args(0), "LogisticRegression")
     // val data = MLUtils.loadLabeledData(sc, args(1))
-    val data = MLUtils.loadSparseLabeledData(sc, args(1), 254)
+    val data = MLUtils.loadSparseLabeledData(sc, args(1), 255)
     val begin = System.nanoTime
     val model = LogisticRegressionWithSGD.train(data, args(3).toInt, args(2).toDouble)
     val end = System.nanoTime
