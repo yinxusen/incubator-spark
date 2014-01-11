ce87a447bb9d612d9fc80f132b7e0b20c2aec5f7
Xusen Yin <yinxusen@gmail.com>
2014-01-11 22:36:44 +0800

    modify run script

--- run-origin.sh
+++ run-origin.sh
@@ -1,1 +1,1 @@
-./spark-class org.apache.spark.mllib.classification.LogisticRegressionWithSGDAlt spark://queen:7077 hdfs://queen:9000/xusen 1.0 20
+./spark-class org.apache.spark.mllib.classification.LogisticRegressionWithSGDAlt spark://queen:7077 hdfs://queen:9000/xusen 1.0 20 2
