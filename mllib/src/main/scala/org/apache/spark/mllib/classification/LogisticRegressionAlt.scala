d64345bcc6f777df9499acb45f731b505ffd7d7f
Xusen Yin <yinxusen@gmail.com>
2014-01-11 11:25:32 +0800

    modify stdin for test

--- mllib/src/main/scala/org/apache/spark/mllib/classification/LogisticRegressionAlt.scala
+++ mllib/src/main/scala/org/apache/spark/mllib/classification/LogisticRegressionAlt.scala
@@ -175,13 +175,13 @@ object LogisticRegressionWithSGDAlt {
   }
 
   def main(args: Array[String]) {
-    if (args.length != 4) {
+    if (args.length != 5) {
       println("Usage: LogisticRegression <master> <input_dir> <step_size> " +
-        "<niters>")
+        "<niters> <mini-split>")
       System.exit(1)
     }
     val sc = new SparkContext(args(0), "LogisticRegression")
-    val data = MLUtils.loadSparseLabeledData(sc, args(1), 255)
+    val data = MLUtils.loadSparseLabeledData(sc, args(1), 255, args(4).toInt)
     val begin = System.nanoTime
     val model = LogisticRegressionWithSGDAlt.train(data, args(3).toInt, args(2).toDouble)
     val end = System.nanoTime
