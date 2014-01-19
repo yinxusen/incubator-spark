8d48bb1b35df0a8de07c02f79ab62a21e3bfed38
sproblvem <sproblvem@gmail.com>
2014-01-19 18:19:15 +0800

    add Job logger

--- mllib/src/main/scala/org/apache/spark/mllib/classification/LogisticRegression.scala
+++ mllib/src/main/scala/org/apache/spark/mllib/classification/LogisticRegression.scala
@@ -25,6 +25,7 @@ import org.apache.spark.mllib.optimization._
 import org.apache.spark.mllib.regression._
 import org.apache.spark.mllib.util.MLUtils
 import org.apache.spark.mllib.util.DataValidators
+import org.apache.spark.scheduler.JobLogger
 
 import org.jblas.DoubleMatrix
 
@@ -181,6 +182,7 @@ object LogisticRegressionWithSGD {
       System.exit(1)
     }
     val sc = new SparkContext(args(0), "LogisticRegression")
+    sc.addSparkListener(new JobLogger())
     // val data = MLUtils.loadLabeledData(sc, args(1))
     val dimension = System.getProperty("spark.default.dimension").toInt
     val data = MLUtils.loadSparseLabeledData(sc, args(1), dimension, args(4).toInt)
--- mllib/src/main/scala/org/apache/spark/mllib/regression/LassoAlt.scala
+++ mllib/src/main/scala/org/apache/spark/mllib/regression/LassoAlt.scala
@@ -21,6 +21,7 @@ import org.apache.spark.{Logging, SparkContext}
 import org.apache.spark.rdd.RDD
 import org.apache.spark.mllib.optimization._
 import org.apache.spark.mllib.util.MLUtils
+import org.apache.spark.scheduler.JobLogger
 
 import org.jblas.DoubleMatrix
 
@@ -203,6 +204,7 @@ object LassoWithSGDAlt {
       System.exit(1)
     }
     val sc = new SparkContext(args(0), "Lasso")
+    sc.addSparkListener(new JobLogger())
     val dimension = System.getProperty("spark.default.dimension").toInt
     // val data = MLUtils.loadLabeledData(sc, args(1))
     val data = MLUtils.loadSparseLabeledData(sc, args(1), dimension, args(5).toInt)
